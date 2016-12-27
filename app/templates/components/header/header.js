55d2c15f5c920befe60b03523dd789aa6552dbc5
zengyanling <939184663@qq.com>
2016-12-27 11:59:57 +0800

    seo template

---
+++ app/templates/components/header/header.js
@@ -0,0 +1,10 @@
+/**
+ * header.js
+ * @version 1.0
+ * @description 头部
+ * @author Xxx
+ */
+
+'use strict';
+
+// 元素查找注意放在 class 为： header
---
+++ app/templates/views/index/index.js
@@ -0,0 +1,10 @@
+/**
+ * index.js
+ * @version 1.0
+ * @description 首页
+ * @author Xxx
+ */
+
+'use strict';
+
+// require('../../cmp/cmpName/cmpName');
